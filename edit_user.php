888bc5e2fe3d7c9d3bf56005e11acc8f32379d0d
Angela-K-C <168468383+Angela-K-C@users.noreply.github.com>
2025-07-11 17:58:14 +0300

    Trying to display the pages

--- edit_user.php
+++ edit_user.php
@@ -41,7 +41,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit User</title>
+    <title>Edit Users</title>
 </head>
 <body>
     <form action="" method="POST">
